cb56991d979c308d82d85501014f4ef040d0913e
黃庭偉 <a0713023@pic.net.tw>
2022-04-08 10:11:29 +0800

    加入專案檔案。

---
+++ EX1_MVC_CRUD_EF/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EX1_MVC_CRUD_EF.Models;
+
+namespace EX1_MVC_CRUD_EF.Controllers
+{
+    public class ProductController : Controller
+    {
+        private NorthwindEntities1 _context = new NorthwindEntities1();
+        private NorthwindEntities1 Context => this._context;
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(Context.Products.ToList());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EX1_MVC_CRUD_EF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EX1_MVC_CRUD_EF.MvcApplication" Language="C#" %>
